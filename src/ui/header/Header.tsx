090f11195e76fe66d6b1a0fb6c9e8ba17157aeb4
Ilya  Stepanov <6967221@gmail.com>
2022-04-20 10:49:50 +0300

    Add header, main + styles.

---
+++ src/ui/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './styles.module.css'
+
+function Header() {
+    return (
+        <div className={s.header}>
+            <div>Trello</div>
+            <div>Hello, ////</div>
+        </div>
+    );
+}
+
+export default Header;
